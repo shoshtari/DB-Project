894244be4645de740b23a67d17e08f2baf81d626
Seyed Ali <wwwwwq37@gmail.com>
2023-07-02 23:25:50 +0330

    update to mysql

--- phase5/backend/myproject/settings.py
+++ phase5/backend/myproject/settings.py
@@ -83,10 +83,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'mydb',
+		'USER': 'root',
+		'PASSWORD': 'admin',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
 
 
